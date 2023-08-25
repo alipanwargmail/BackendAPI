e6285c0d7ed3031419ab9d50f097615f9a7e3883
Anwar Raharja <alipanwar@yahoo.com>
2023-08-26 00:07:15 +0700

    A

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -3,7 +3,7 @@ const bcryptjs = require('bcryptjs')
 var USERS = require('../../arrayusers.js')
 
 exports.handler = async function (event, context) {
-  
+
   let user = JSON.parse(event.body).username
   let pass = JSON.parse(event.body).password
   let json_msg = "";
@@ -11,56 +11,58 @@ exports.handler = async function (event, context) {
   var breakloop = false
   let log = event.body + "|" //JSON.stringify(body)+"|";
   log += "username: " + user + " password: " + pass + "|USERS.length: " + USERS.length + "|"
-  
-   // json_msg = '{log="'+log+'"}'
 
-    while (i < USERS.length && !breakloop) {
-      let value = USERS[i]
-      log += USERS[i].username+ "|<"+JSON.parse(event.body).username+">|"+user+"|"
-      console.log(USERS[i].username+ "|<"+JSON.parse(event.body).username+">|"+user+"|")
-      
-      if (USERS[i].username == user) {
-        log += "username " + user + " found|"
-        console.log("ok nih")
-        
-        bcryptjs.compare(pass, value.password, (err, valid) => {
-          if (err) {
-            log += "Error on password validation|";
-            json_msg = '{ result: "Not Ok", message: "Error on password validation", log="' + log + '"}';
-            //res.status(200).json({ result: "Not Ok", message: "Error on password validation" });
-          }
-          if (valid) {
-            //const body = event.body;
-            const ptoken = "TOKEN"//jwt.sign({ user: body }, KEY_TOKEN);
-            json_msg = '{ result: "OK", message: "Login OK" }'
-            //json_msg = '{ result: "OK", message: "Login OK", user_id: value.id, username: value.username, role_user: value.role_user, email: value.email, token: '+ptoken+', log="'+log+'" }';
-            //res.status(200).json({ result: "OK", message: "Login OK", user_id: value.id, username: value.username, role_user: value.role_user, email: value.email, token: ptoken });
-          } else {
-            json_msg = '{ result: "Not Ok", message: "Incorrect username or password", log="' + log + '" }';
-            //res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
-          }
-        });
-      
-        log += "about to break i: " + i + "|"
-        console.log("about to break")
-        break;
+  // json_msg = '{log="'+log+'"}'
+
+  while (i < USERS.length && !breakloop) {
+    let value = USERS[i]
+    log += USERS[i].username + "|<" + JSON.parse(event.body).username + ">|" + user + "|"
+    console.log(USERS[i].username + "|<" + JSON.parse(event.body).username + ">|" + user + "|")
+
+    if (USERS[i].username == user) {
+      log += "username " + user + " found|"
+      console.log("ok nih")
+
+      //bcryptjs.compare(pass, value.password, (err, valid) => {
+      let valid = bcryptjs.compareSync(pass, value.password)
+      /*
+      if (err) {
+        log += "Error on password validation|";
+        json_msg = '{ result: "Not Ok", message: "Error on password validation", log="' + log + '"}';
+        //res.status(200).json({ result: "Not Ok", message: "Error on password validation" });
+      }*/
+      if (valid) {
+        console.log("valid")
+        //const body = event.body;
+        const ptoken = "TOKEN"//jwt.sign({ user: body }, KEY_TOKEN);
+        json_msg = '{ result: "OK", message: "Login OK" }'
+        //json_msg = '{ result: "OK", message: "Login OK", user_id: value.id, username: value.username, role_user: value.role_user, email: value.email, token: '+ptoken+', log="'+log+'" }';
+        //res.status(200).json({ result: "OK", message: "Login OK", user_id: value.id, username: value.username, role_user: value.role_user, email: value.email, token: ptoken });
+      } else {
+        json_msg = '{ result: "Not Ok", message: "Incorrect username or password", log="' + log + '" }';
+        //res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
       }
-      else {
-        i++
-      } 
-    
+      log += "about to break i: " + i + "|"
+      console.log("about to break")
+      break;
     }
-    //json_msg = '{log="'+log+'"}'
-    console.log('keluar loop')
-    if (i == USERS.length) {
-      log += "i == USERS.length|";
-      //console.log(login_ok)
-      json_msg = '{ result: "Not Ok", message: "Incorrect username or password", log="' + log + '" }';
-      //res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
+    // );
+    else {
+      i++
     }
-    else{
-      console.log('masuk sini')
-    }
+
+  }
+  //json_msg = '{log="'+log+'"}'
+  console.log('keluar loop')
+  if (i == USERS.length) {
+    log += "i == USERS.length|";
+    //console.log(login_ok)
+    json_msg = '{ result: "Not Ok", message: "Incorrect username or password", log="' + log + '" }';
+    //res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
+  }
+  else {
+    console.log('masuk sini')
+  }
 
   return {
     statusCode: 200,
