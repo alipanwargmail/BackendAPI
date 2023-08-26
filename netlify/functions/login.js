7065f0afedb3fb141867f22f1a5fd93b946f12eb
Anwar Raharja <alipanwar@yahoo.com>
2023-08-26 11:15:36 +0700

    A

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -9,7 +9,7 @@ exports.handler = async function (event, context) {
       statusCode: 200,
       headers: {
         "Access-Control-Allow-Origin" : "*",
-        "Access-Control-Allow-Headers" : "Content-Type, Authorization, Origin",
+        "Access-Control-Allow-Headers" : "Content-Type, Authorization, Origin, Access-Control-Allow-Origin",
         "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE,OPTIONS",
         "Content-Type" : "application/json",
       },
