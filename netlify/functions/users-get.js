86b0d769ff8879236237b44b6929ff90c796cd41
Anwar Raharja <alipanwar@yahoo.com>
2023-08-26 13:06:24 +0700

    A

--- netlify/functions/users-get.js
+++ netlify/functions/users-get.js
@@ -2,29 +2,49 @@ var USERS = require('../../arrayusers.js')
 
 exports.handler = async function (event, context) {
 
-    if (event.httpMethod == 'OPTIONS') {
-        return {
-          statusCode: 200,
-          headers: {
-            "Access-Control-Allow-Origin" : "*",
-            "Access-Control-Allow-Headers" : "Content-Type, Authorization, Origin, Access-Control-Allow-Origin",
-            "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE,OPTIONS",
-            "Content-Type" : "application/json",
-          },
-        };
+  if (event.httpMethod == 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, Origin, Access-Control-Allow-Origin",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTIONS",
+        "Content-Type": "application/json",
+      },
+    };
+  }
+  else {
+    let paramid = event.queryStringParameters.id;
+    console.log(event.queryStringParameters)
+    console.log(paramid)
+    let retval = null
+    let i = 0
+    while (i < USERS.length) {
+      const value = USERS[i]
+      if (value.id == req.params.id) {
+
+        retval = value;
+        //res.status(200).json(value);
+        break;
       }
       else {
-
-    return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin" : "*",
-            "Access-Control-Allow-Headers" : "Content-Type, Authorization, Origin, Access-Control-Allow-Origin",
-            "Access-Control-Allow-Methods" : "GET, POST, PUT, DELETE,OPTIONS",
-            "Content-Type" : "application/json",
-
-          },                        
-          body: JSON.stringify(USERS),
-      };
+        i++;
+      }
     }
+    if (i == USERS.length) {
+      //return res.status(200).json("");
+      retval = "";
+    }
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, Origin, Access-Control-Allow-Origin",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTIONS",
+        "Content-Type": "application/json",
+
+      },
+      body: JSON.stringify(retval),
+    };
+  }
 }
