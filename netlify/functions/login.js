e7d4e0693f3961860fb2e0777553bcb2005557f7
alipanwargmail <alipanwar@gmail.com>
2023-08-28 15:01:20 +0700

    A

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -15,7 +15,7 @@ exports.handler = async function (event, context) {
       },
     };
   }
-  else {
+  else if(event.httpMethod == 'POST'){
     console.log(event.httpMethod)
     console.log(process.env.DB_URL)
     let user = JSON.parse(event.body).username
@@ -66,6 +66,18 @@ exports.handler = async function (event, context) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(json_msg),
+    };
+  }
+  else{
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, Origin",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTIONS",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(""),
     };
   }
 }
