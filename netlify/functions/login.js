46f6e9f7a1a6e25124762b5de011c1350c31e124
Anwar Raharja <alipanwar@yahoo.com>
2023-08-28 07:07:01 +0700

    A

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -24,28 +24,23 @@ exports.handler = async function (event, context) {
     let json_msg = "";
     const client = await pg.connect()
     try {
-      client.query('select id, "username", "password", "role_user", "email" from "users" where "username"=$1', 
-      [user], (err, result) => {
-        if (err) {
-          status = 401;
-          json_msg = { result: "ERR", message: "Unauthorized" }
-          //return res.status(401).json({ result: "ERR", message: "Unauthorized" });
-        }
-        else {
-          console.log(result.rowCount)
-          if (result.rowCount > 0) {
-            bcryptjs.compare(pass, result.rows[0].password, (err, valid) => {
-              if (err) {
-                console.log("Error on password validation");
-                json_msg = '{ result: "Not Ok", message: "Error on password validation" }'
-                //return res.status(200).json({ result: "Not Ok", message: "Error on password validation" });
-              }
+      client.query('select id, "username", "password", "role_user", "email" from "users" where "username"=$1',
+        [user], (err, result) => {
+          if (err) {
+            status = 401;
+            json_msg = { result: "ERR", message: "Unauthorized" }
+            //return res.status(401).json({ result: "ERR", message: "Unauthorized" });
+          }
+          else {
+            console.log(result.rowCount)
+            if (result.rowCount > 0) {
+              const valid = bcryptjs.compareSync(pass, result.rows[0].password)
               if (valid) {
                 console.log('User [' + user + '] has logged in.');
                 const body = event.body;
                 const ptoken = jwt.sign({ user: body }, "TOP_SECRET");
                 client.release()
-                json_msg = '{ result: "OK", message: "Login OK", user_id: '+result.rows[0].id+', username: '+result.rows[0].username+', role_user: '+result.rows[0].role_user+', email: '+result.rows[0].email+', token: '+ptoken+' }'
+                json_msg = '{ result: "OK", message: "Login OK", user_id: ' + result.rows[0].id + ', username: ' + result.rows[0].username + ', role_user: ' + result.rows[0].role_user + ', email: ' + result.rows[0].email + ', token: ' + ptoken + ' }'
                 //return res.status(200).json({ result: "OK", message: "Login OK", user_id: result.rows[0].id, username: result.rows[0].username, role_user: result.rows[0].role_user, email: result.rows[0].email, token: ptoken });
               } else {
 
@@ -53,19 +48,19 @@ exports.handler = async function (event, context) {
                 json_msg = '{ result: "Not Ok", message: "Incorrect username or password" }'
                 //return res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
               }
-            });
-          }
-          else {
 
-            client.release()
-            json_msg = '{ result: "Not Ok", message: "Incorrect username or password2" }'
-            //return res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
+            }
+            else {
+
+              client.release()
+              json_msg = '{ result: "Not Ok", message: "Incorrect username or password2" }'
+              //return res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
+            }
           }
-        }
-      })
+        })
     }
     catch (e) {
-      json_msg = '{ result: "Error", message: "Server Error" '+ e +' }'
+      json_msg = '{ result: "Error", message: "Server Error" ' + e + ' }'
       //return res.status(500).json({ result: "Error", message: "Server Error" + e })
     }
     console.log("login LEWAT SINI")
