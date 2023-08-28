0979d5d71349512a66fd4e5c74ec1bbd31b8580f
alipanwargmail <alipanwar@gmail.com>
2023-08-28 11:49:22 +0700

    A

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -18,7 +18,7 @@ exports.handler = async function (event, context) {
   }
   else {
     console.log(event.httpMethod)
-    
+
     let user = JSON.parse(event.body).username
     let pass = JSON.parse(event.body).password
     console.log(user)
@@ -26,49 +26,29 @@ exports.handler = async function (event, context) {
     let json_msg = "";
     //const client = await pg.connect()
     try {
-      
-      const result = await db.query('select id, "username", "password", "role_user", "email" from "users" where "username"=$1',[user])
+
+      const result = await db.query('select id, "username", "password", "role_user", "email" from "users" where "username"=$1', [user])
       console.log(result)
       json_msg = result
-      /*
-      client.query('select id, "username", "password", "role_user", "email" from "users" where "username"=$1',
-        [user], (err, result) => {
-          if (err) {
-            console.log("err query")
-            status = 401;
-            json_msg = { result: "ERR", message: "Unauthorized" }
-            //return res.status(401).json({ result: "ERR", message: "Unauthorized" });
-          }
-          else {
-            console.log(result.rowCount)
-            if (result.rowCount > 0) {
-              const valid = bcryptjs.compareSync(pass, result.rows[0].password)
-              if (valid) {
-                console.log('User [' + user + '] has logged in.');
-                const body = event.body;
-                const ptoken = jwt.sign({ user: body }, "TOP_SECRET");
-                client.release()
-                json_msg = '{ result: "OK", message: "Login OK", user_id: ' + result.rows[0].id + ', username: ' + result.rows[0].username + ', role_user: ' + result.rows[0].role_user + ', email: ' + result.rows[0].email + ', token: ' + ptoken + ' }'
-                //return res.status(200).json({ result: "OK", message: "Login OK", user_id: result.rows[0].id, username: result.rows[0].username, role_user: result.rows[0].role_user, email: result.rows[0].email, token: ptoken });
-              } else {
+      if (result.rowCount > 0) {
+        const valid = bcryptjs.compareSync(pass, result.rows[0].password)
+        if (valid) {
+          console.log('User [' + user + '] has logged in.');
+          const body = event.body;
+          const ptoken = jwt.sign({ user: body }, "TOP_SECRET");
+          json_msg = '{ result: "OK", message: "Login OK", user_id: ' + result.rows[0].id + ', username: ' + result.rows[0].username + ', role_user: ' + result.rows[0].role_user + ', email: ' + result.rows[0].email + ', token: ' + ptoken + ' }'
+          //return res.status(200).json({ result: "OK", message: "Login OK", user_id: result.rows[0].id, username: result.rows[0].username, role_user: result.rows[0].role_user, email: result.rows[0].email, token: ptoken });
+        } else {
 
-                client.release()
-                json_msg = '{ result: "Not Ok", message: "Incorrect username or password" }'
-                //return res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
-              }
+          json_msg = '{ result: "Not Ok", message: "Incorrect username or password" }'
+          //return res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
+        }
+      }
+      else {
 
-            }
-            else {
-
-              client.release()
-              json_msg = '{ result: "Not Ok", message: "Incorrect username or password2" }'
-              //return res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
-            }
-          }
-        })
-        */
-      
-        console.log("BLAH BLAH")
+        json_msg = '{ result: "Not Ok", message: "Incorrect username or password2" }'
+        //return res.status(200).json({ result: "Not Ok", message: "Incorrect username or password" });
+      }
     }
     catch (e) {
       json_msg = '{ result: "Error", message: "Server Error" ' + e + ' }'
