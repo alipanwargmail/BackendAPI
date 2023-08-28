5b80461b52c2cd6ff53b291d585baceb03a593db
Anwar Raharja <alipanwar@yahoo.com>
2023-08-28 07:59:42 +0700

    A

--- netlify/functions/dbusingpgpromise.js
+++ netlify/functions/dbusingpgpromise.js
@@ -1,2 +1,2 @@
 const pgp = require('pg-promise')();
-module.exports = pgp({ "DATABASE": "postgres://ixiihrnc:HP06So35UiQcqRXFXVlUZ6Us8NNZC5gH@floppy.db.elephantsql.com/ixiihrnc"});
+module.exports = pgp({ "DATABASE": "postgres://ixiihrnc:HP06So35UiQcqRXFXVlUZ6Us8NNZC5gH@floppy.db.elephantsql.com:5432/ixiihrnc"});
--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -26,6 +26,7 @@ exports.handler = async function (event, context) {
     let json_msg = "";
     //const client = await pg.connect()
     try {
+      
       const result = await pgp.query('select id, "username", "password", "role_user", "email" from "users" where "username"=$1',[user])
       console.log(result)
       json_msg = result
@@ -66,6 +67,7 @@ exports.handler = async function (event, context) {
           }
         })
         */
+      
         console.log("BLAH BLAH")
     }
     catch (e) {
