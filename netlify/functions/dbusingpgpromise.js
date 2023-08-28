6e92e282eb2cb146de59d0def567b621fbac8ca9
Anwar Raharja <alipanwar@yahoo.com>
2023-08-28 08:19:34 +0700

    A

--- netlify/functions/dbusingpgpromise.js
+++ netlify/functions/dbusingpgpromise.js
@@ -1,6 +1,6 @@
-const pgp = require('pg-promise')
+const pgp = require('pg-promise')()
 const db = pgp({
-    'DATABASE': 'postgres://ixiihrnc:HP06So35UiQcqRXFXVlUZ6Us8NNZC5gH@floppy.db.elephantsql.com:5432/ixiihrnc'
+    "DATABASE": "postgres://ixiihrnc:HP06So35UiQcqRXFXVlUZ6Us8NNZC5gH@floppy.db.elephantsql.com:5432/ixiihrnc"
 })
 
 module.exports = db
