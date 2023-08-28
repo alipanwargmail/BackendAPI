ae8fdef59115759f98c6ff62c33aabb60a0059bc
Anwar Raharja <alipanwar@yahoo.com>
2023-08-28 12:38:21 +0700

    A

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -31,8 +31,8 @@ exports.handler = async function (event, context) {
       const result = await db.query('select id, "username", "password", "role_user", "email" from "users" where "username"=$1', [user])
       console.log(result)
       json_msg = result
-      console.log(JSON.parse(result).rowCount)
-      if (result.rowCount > 0) {
+      console.log(result.length)
+      if (result.length > 0) {
         console.log(result.rows[0].password)
         const valid = bcryptjs.compareSync(pass, result.rows[0].password)
         if (valid) {
