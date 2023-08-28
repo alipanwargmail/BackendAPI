ce188ab2905783f00d7d69307be6cc870e79cbff
Anwar Raharja <alipanwar@yahoo.com>
2023-08-28 08:37:54 +0700

    A

--- netlify/functions/dbusingpgpromise.js
+++ netlify/functions/dbusingpgpromise.js
@@ -2,17 +2,18 @@
 
 const initOptions = {
 
-    // pg-promise initialization options...
-
-    connect(e) {
-        const cp = e.client.connectionParameters;
-        console.log('Connected to database:', cp.database);
+    error: function (error, e) {
+        if (e.cn) {
+            // A connection-related error;
+            console.log("CN:", e.cn);
+            console.log("EVENT:", error.message);
+        }
     }
 
 };
 const pgp = require('pg-promise')(initOptions)
-const db = pgp({
-    "DATABASE": "postgres://ixiihrnc:HP06So35UiQcqRXFXVlUZ6Us8NNZC5gH@floppy.db.elephantsql.com:5432/ixiihrnc"
-})
+const db = pgp(
+    "postgres://ixiihrnc:HP06So35UiQcqRXFXVlUZ6Us8NNZC5gH@floppy.db.elephantsql.com:5432/ixiihrnc"
+)
 
 module.exports = db
