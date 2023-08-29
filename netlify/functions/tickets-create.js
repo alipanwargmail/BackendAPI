324dbaebf66ab50462a3a29195ecc97e9a01b583
alipanwargmail <alipanwar@gmail.com>
2023-08-29 17:14:20 +0700

    A

--- netlify/functions/tickets-create.js
+++ netlify/functions/tickets-create.js
@@ -22,12 +22,14 @@ exports.handler = async function (event, context) {
     let ldeskripsi = JSON.parse(event.body).deskripsi
     let lpriority = JSON.parse(event.body).priority
     let lemail = JSON.parse(event.body).email
+    let lphone_no = JSON.parse(event.body).phone_no
     console.log(luser_id)
     console.log(lusername)
     console.log(ltitle)
     console.log(ldeskripsi)
     console.log(lpriority)
     console.log(lemail)
+    console.log(lphone_no)
     try {
       console.log("find agent===============")
       const results = await db.query("select a.id,a.username,a.email,a.phone_no,count(*) from users a left join tickets b \
@@ -39,8 +41,10 @@ exports.handler = async function (event, context) {
       var handler_user_id = results[0].id;
       var handler_username = results[0].username;
       var handler_email = results[0].email;
+      var handler_phone_no = results[0].phone_no
       console.log(handler_user_id)
       console.log(handler_username)
+      console.log(handler_email)
       console.log(handler_email)
       var results2 = await db.query("INSERT INTO tickets (user_id, username, email, title, deskripsi, priority, handler_user_id,handler_username, status, created_at, updated_at) VALUES ($1, $2, $3, $4, $5,$6,$7,$8, 'OPEN', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) RETURNING *",
         [luser_id, lusername, lemail, ltitle, ldeskripsi, lpriority, handler_user_id, handler_username])
@@ -81,6 +85,37 @@ exports.handler = async function (event, context) {
           "Created at: " + results2[0].created_at + "\n"
       })
       console.log('Email for agent sent: ' + info2.response);
+
+      var to = lphone_no
+      const type = "text"
+      var text = 'Your ticket has been created with ID: '+results2[0].id+' and handle by ' + handler_username
+      const useTyping = "true"
+      await axios.post(process.env.WA_URL, {to, type, text, useTyping}, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'authorization': 'Bearer ' + process.env.WA_TOKEN
+        }
+      })
+      //console.log(info3);
+      callback(null, { statusCode: 200, body: JSON.stringify({}) })
+      
+      to = handler_phone_no
+      text = "Your ticket has been opened with ID: "+results2[0].id+" and dispatched to you (" + handler_username + ") with detail: \n" +
+      "Requester: "+ lusername + "\n" +
+      "Title: " + ltitle + "\n" +
+      "Deskripsi: " + ldeskripsi + "\n" +
+      "Priority: " + lpriority + "\n" +
+      "Created at: " + results2[0].created_at
+      await axios.post(process.env.WA_URL, {to, type, text, useTyping}, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'authorization': 'Bearer ' + process.env.WA_TOKEN
+        }
+      })
+      //console.log(info3);
+      callback(null, { statusCode: 200, body: JSON.stringify({}) })      
     }
     catch (error) {
       callback(error);
