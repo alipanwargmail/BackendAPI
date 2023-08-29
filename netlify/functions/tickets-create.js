085085a746c1bb1c54d26b5df418ae46dde06792
Anwar Raharja <alipanwar@yahoo.com>
2023-08-29 08:23:18 +0700

    A

--- netlify/functions/tickets-create.js
+++ netlify/functions/tickets-create.js
@@ -1,4 +1,5 @@
 const db = require('./dbusingpgpromise.js');
+var nodemailer = require('nodemailer')
 
 exports.handler = async function (event, context) {
   console.log(event.httpMethod)
@@ -41,8 +42,51 @@ exports.handler = async function (event, context) {
       console.log(handler_user_id)
       console.log(handler_username)
       console.log(handler_email)
-
-      json_msg = results;
+      const results2= await db.query("INSERT INTO tickets (user_id, username, email, title, deskripsi, priority, handler_user_id,handler_username, status, created_at, updated_at) VALUES ($1, $2, $3, $4, $5,$6,$7,$8, 'OPEN', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) RETURNING *",
+      [luser_id, lusername, lemail, ltitle, ldeskripsi, lpriority, handler_user_id, handler_username])      
+      json_msg = results2;
+      var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'ikeltiga@gmail.com',
+          //pass: 'Isupportkelompok32'
+          pass: 'ewxsofcnulplgjhn'
+        }
+      });
+      var usermail = {
+        from: 'isupport-kelompok3',
+        to: lemail,
+        subject: 'New ticket created with ID: ' + results2[0].id,
+        text: 'Your ticket has been created and handle by ' + handler_username
+      }
+      transporter.sendMail(usermail, function (error, info) {
+      if (error) {
+        console.log(error);
+        //res.json({ status: "ERR", message: error })
+      } else {
+        console.log('Email for user sent: ' + info.response);
+        var agentmail = {
+          from: 'isupport-kelompok3',
+          to: handler_email,
+          subject: 'New ticket dispatched to you with ID: ' + results2[0].id,
+          text: 'New ticket has been opened and dispatched to you (' + handler_username + ") with detail \n \
+        Requester: "+ lusername + "\n" +
+            "Title: " + ltitle + "\n" +
+            "Deskripsi: " + ldeskripsi + "\n" +
+            "Priority: " + lpriority + "\n" +
+            "Created at: " + results2[0].created_at + "\n"
+        }
+        transporter.sendMail(agentmail, function (error, info) {
+          if (error) {
+            console.log(error);
+            //res.json({ status: "ERR", message: error })
+          } else {
+            console.log('Email for agent sent: ' + info.response);
+            return res.status(201).json({ result: "OK", message: "Tickets added with ID: " + results2[0].id })
+            //res.json({ status: "OK", message: info.response })
+          }
+        })
+      }})
     }
     catch (e) {
       json_msg.result = "Error"
