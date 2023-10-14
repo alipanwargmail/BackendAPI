8dde19695a580b8d7606d7846a156bfbe0096978
alipanwargmail <alipanwar@gmail.com>
2023-10-14 14:23:34 +0700

    A

---
+++ netlify/functions/emails-getall.js
@@ -0,0 +1,43 @@
+const db = require('./dbusingpgpromise.js');
+
+exports.handler = async function (event, context) {
+  let json_msg = {}
+  if (event.httpMethod == 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, Origin, Access-Control-Allow-Origin",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTIONS",
+        "Content-Type": "application/json",
+      },
+    };
+  }
+  else {
+    console.log(event.httpMethod)
+    console.log(process.env.DB_URL)
+    
+    try {
+      const result = await db.query('select * from emails order by id')
+      console.log(result)
+      json_msg = result;
+    }
+    catch (e) {
+      json_msg.result = "Error"
+      json_msg.message = "Server Error "+e
+      console.log("Server Error "+e)
+      //json_msg = '{ result: "Error", message: "Server Error" ' + e + ' }'
+    }
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, Origin, Access-Control-Allow-Origin",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTIONS",
+        "Content-Type": "application/json",
+
+      },
+      body: JSON.stringify(json_msg),
+    };
+  }
+}
